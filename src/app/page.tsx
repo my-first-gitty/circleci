e71b76ca74a27e7be55b6af2531d5f651f727745
satishlinkites <satishpatidar@linkites.com>
2024-01-15 15:37:32 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
             rel="noopener noref errer"
           >
             By{' '} 
+            
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
